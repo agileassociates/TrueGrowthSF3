390e76292e1b935998e7691bd3a710500f02f067
teagolf <teagolf_blog@protonmail.com>
2020-12-11 23:40:21 -0500

    TrueGrowth4

---
+++ InstagramApp/ViewControllers/HomeViewController.swift
@@ -0,0 +1,31 @@
+//
+//  HomeViewController.swift
+//  InstagramApp
+//
+//  Created by Johny Babylon on 12/11/20.
+//  Copyright © 2020 Gwinyai Nyatsoka. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ InstagramApp/ViewControllers/NewPostViewController.swift
@@ -0,0 +1,30 @@
+//
+//  NewPostViewController.swift
+//  InstagramApp
+//
+//  Created by Johny Babylon on 12/11/20.
+//  Copyright © 2020 Gwinyai Nyatsoka. All rights reserved.
+//
+
+import UIKit
+
+class NewPostViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
